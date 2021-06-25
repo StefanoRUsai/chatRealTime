3c6bb8bb2e7224e10226a3ef3b11a5eac8043736
Stefano Raimondo Usai <stefanoraimondo.usai@spindox.it>
2021-06-25 16:42:34 +0200

    initial frontend configuration

--- chatfrontend/src/index.js
+++ chatfrontend/src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./store/reducer/index";
+
+const store = createStore(rootReducers);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
